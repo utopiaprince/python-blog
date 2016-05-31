d88768c79a2cee7a79b739933fe8a33f4f3641c7
gang.cheng <gang.cheng1987@gmail.com>
2016-05-31 21:23:57 +0800

    in_0531_add: 编写Web App骨架

---
+++ app.py
@@ -0,0 +1,14 @@
+import logging
+import asyncio
+import os
+import json
+import time
+from datetime import datetime
+
+logging.basicConfig(level=logging.INFO)
+
+ab = 5id
+
+
+def func(void):
+    pass
---
+++ readme.md
@@ -0,0 +1,1 @@
+# this is python 3.0 webapp demo
